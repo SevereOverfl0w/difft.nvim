5406940259362f4b7d7dc80af82d1f8984047d91
Dominic Monroe <dominic@dominic.io>
2026-06-08 06:04:37 +0100

    Switch from ]h to ]c
    Matches built-ins better

--- ftplugin/difftastic.lua
+++ ftplugin/difftastic.lua
@@ -1,7 +1,7 @@
-vim.keymap.set('n', ']h', function()
+vim.keymap.set('n', ']c', function()
     require('difft').jump_hunk(1)
 end, {buffer = true, desc = 'Next difftastic hunk'})
 
-vim.keymap.set('n', '[h', function()
+vim.keymap.set('n', '[c', function()
     require('difft').jump_hunk(-1)
 end, {buffer = true, desc = 'Previous difftastic hunk'})
